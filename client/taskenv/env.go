5830f2800c732dc6781ec7b7c3ca25e66625b276
James Rasell <jrasell@hashicorp.com>
2022-01-10 16:56:30 +0100

    client: expose network namespace CNI config as task env vars.
    This change exposes CNI configuration details of a network
    namespace as environment variables. This allows a task to use
    these value to configure itself; a potential use case is to run
    a Raft application binding to IP and Port details configured using
    the bridge network mode.

--- client/taskenv/env.go
+++ client/taskenv/env.go
@@ -105,6 +105,11 @@ const (
 	// UpstreamPrefix is the prefix for passing upstream IP and ports to the alloc
 	UpstreamPrefix = "NOMAD_UPSTREAM_"
 
+	// AllocPrefix is a general purpose alloc prefix. It is currently used as
+	// the env var prefix used to export network namespace information
+	// including IP, Port, and interface.
+	AllocPrefix = "NOMAD_ALLOC_"
+
 	// VaultToken is the environment variable for passing the Vault token
 	VaultToken = "VAULT_TOKEN"
 
@@ -424,6 +429,9 @@ type Builder struct {
 	// and affect network env vars.
 	networks []*structs.NetworkResource
 
+	networkStatus  *structs.AllocNetworkStatus
+	allocatedPorts structs.AllocatedPorts
+
 	// hookEnvs are env vars set by hooks and stored by hook name to
 	// support adding/removing vars from multiple hooks (eg HookA adds A:1,
 	// HookB adds A:2, HookA removes A, A should equal 2)
@@ -535,6 +543,12 @@ func (b *Builder) buildEnv(allocDir, localDir, secretsDir string,
 
 	// Build the Consul Connect upstream env vars
 	buildUpstreamsEnv(envMap, b.upstreams)
+
+	// Build the network namespace information if we have the required detail
+	// available.
+	if b.networkStatus != nil && b.allocatedPorts != nil {
+		addNetNamespacePort(envMap, b.allocatedPorts, b.networkStatus)
+	}
 
 	// Build the Vault Token
 	if b.injectVaultToken && b.vaultToken != "" {
@@ -777,6 +791,7 @@ func (b *Builder) setAlloc(alloc *structs.Allocation) *Builder {
 
 		// Add any allocated host ports
 		if alloc.AllocatedResources.Shared.Ports != nil {
+			b.allocatedPorts = alloc.AllocatedResources.Shared.Ports
 			addPorts(b.otherPorts, alloc.AllocatedResources.Shared.Ports)
 		}
 	}
@@ -921,6 +936,13 @@ func (b *Builder) setUpstreamsLocked(upstreams []structs.ConsulUpstream) *Builde
 	return b
 }
 
+func (b *Builder) SetNetworkStatus(netStatus *structs.AllocNetworkStatus) *Builder {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.networkStatus = netStatus
+	return b
+}
+
 // buildUpstreamsEnv builds NOMAD_UPSTREAM_{IP,PORT,ADDR}_{destination} vars
 func buildUpstreamsEnv(envMap map[string]string, upstreams []structs.ConsulUpstream) {
 	// Proxy sidecars always bind to localhost
@@ -936,6 +958,18 @@ func buildUpstreamsEnv(envMap map[string]string, upstreams []structs.ConsulUpstr
 		envMap[UpstreamPrefix+"ADDR_"+cleanName] = net.JoinHostPort(ip, port)
 		envMap[UpstreamPrefix+"IP_"+cleanName] = ip
 		envMap[UpstreamPrefix+"PORT_"+cleanName] = port
+	}
+}
+
+// addNetNamespacePort builds NOMAD_NET_NAMESPACE_{IP,PORT,ADDR}_{port_label}
+// vars.
+func addNetNamespacePort(envMap map[string]string, p structs.AllocatedPorts, netStatus *structs.AllocNetworkStatus) {
+	for _, allocatedPort := range p {
+		portStr := strconv.Itoa(allocatedPort.To)
+		envMap[AllocPrefix+"INTERFACE_"+allocatedPort.Label] = netStatus.InterfaceName
+		envMap[AllocPrefix+"IP_"+allocatedPort.Label] = netStatus.Address
+		envMap[AllocPrefix+"PORT_"+allocatedPort.Label] = portStr
+		envMap[AllocPrefix+"ADDR_"+allocatedPort.Label] = net.JoinHostPort(netStatus.Address, portStr)
 	}
 }
 
